c61dd877f45fb79863c543f73f1d45e307343060
Jim Vickery <jimrv8@yahoo.com>
2020-06-14 17:14:28 -0500

    add code to get Lead then contact, then User email

--- lib/mailkick.rb
+++ lib/mailkick.rb
@@ -22,7 +22,8 @@ require "mailkick/engine" if defined?(Rails)
 module Mailkick
   mattr_accessor :services, :user_method, :secret_token, :mount
   self.services = []
-  self.user_method = ->(email) { User.where(email: email).first rescue nil }
+  # added if conditional to get Lead then contact, then User email
+  self.user_method = ->(email) { Lead.where(email: email).first rescue nil if Lead.where(email: email).first.nil? Contact.where(email: email).first rescue nil end if Contact.where(email: email).first.nil? User.where(email: email).first rescue nil end }
   self.mount = true
 
   def self.fetch_opt_outs
