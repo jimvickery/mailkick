a87bfd2ce739f90c470b8fdf44aeb38665e0ffae
Jim Vickery <jimrv8@yahoo.com>
2020-06-15 00:18:45 -0500

    reverted user_method

--- lib/mailkick.rb
+++ lib/mailkick.rb
@@ -23,11 +23,6 @@ module Mailkick
   mattr_accessor :services, :user_method, :secret_token, :mount
   self.services = []
   self.user_method = ->(email) { Contact.where(email: email).first }
-  
-  if Mailkick.user_method.nil?
-    self.user_method = ->(email) { Lead.where(email: email).first rescue nil }
-  end
-
   self.mount = true
 
   def self.fetch_opt_outs
