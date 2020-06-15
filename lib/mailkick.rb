94d475a693e20cd3a77edc36238d6e7746946134
Jim Vickery <jimrv8@yahoo.com>
2020-06-14 23:25:59 -0500

    modified user_method

--- lib/mailkick.rb
+++ lib/mailkick.rb
@@ -22,7 +22,9 @@ require "mailkick/engine" if defined?(Rails)
 module Mailkick
   mattr_accessor :services, :user_method, :secret_token, :mount
   self.services = []
-  self.user_method = ->(email) { Contact.where(email: email).first : Lead.where(email: email).first rescue nil }
+  self.user_method = ->(email) { Contact.where(email: email).first if email.present? }
+  self.user_method = ->(email) { Lead.where(email: email).first if email.present? }
+  
   self.mount = true
 
   def self.fetch_opt_outs
