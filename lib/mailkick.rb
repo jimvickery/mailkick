de1d5fa92ca3b1d58d3e818eef3e73ffd42d6abd
Jim Vickery <jimrv8@yahoo.com>
2020-06-14 22:53:58 -0500

    attempt to add leads to user_method

--- lib/mailkick.rb
+++ lib/mailkick.rb
@@ -22,7 +22,7 @@ require "mailkick/engine" if defined?(Rails)
 module Mailkick
   mattr_accessor :services, :user_method, :secret_token, :mount
   self.services = []
-  self.user_method = ->(email) { Contact.where(email: email).first rescue nil }
+  self.user_method = ->(email) { Contact.where(email: email).first : Lead.where(email: email).first rescue nil }
   self.mount = true
 
   def self.fetch_opt_outs
