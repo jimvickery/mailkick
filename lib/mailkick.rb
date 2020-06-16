3639fd2be3b7469e9ed3f38d639a760d0dfd5b28
Jim Vickery <jimrv8@yahoo.com>
2020-06-15 22:49:39 -0500

    reverted user_method to contact model

--- lib/mailkick.rb
+++ lib/mailkick.rb
@@ -22,10 +22,7 @@ require "mailkick/engine" if defined?(Rails)
 module Mailkick
   mattr_accessor :services, :user_method, :secret_token, :mount
   self.services = []
-  self.user_method = ->(email) { Contact.where(email: email).first }
-  if email.nil?
-    self.user_method = ->(email) { Lead.where(email: email).first rescue nil}
-  end
+  self.user_method = ->(email) { Contact.where(email: email).first rescue nil}
   self.mount = true
 
   def self.fetch_opt_outs
